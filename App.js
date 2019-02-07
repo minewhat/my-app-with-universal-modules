1de539122881795194d97ac54eec0fe8aaf7a493
Ram Ganesan <ramg8x@gmail.com>
2019-02-07 20:22:17 +0530

    Font added to new repo

--- App.js
+++ App.js
@@ -1,17 +1,40 @@
 import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
-import { Constants } from '@universal-modules/react-native';
+import { Platform, StyleSheet, Text, ScrollView, View } from 'react-native';
+import { Constants } from '@universal-modules/react-native-platform';
 import { Localization } from 'expo-localization';
+import * as Font from 'expo-font';
+import * as Asset from 'expo-asset';
 
 export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>{Localization.locale}</Text>
-        <Text>{JSON.stringify(Constants)}</Text>
+        <Text style={styles.quote}>Because of your smile, you make life more beautiful.</Text>
+        <ScrollView>
+          <Text style={styles.welcome}>{Localization.locale}</Text>
+          <Text>{JSON.stringify(Constants)}</Text>
+        </ScrollView>
       </View>
     );
   }
+
+  async componentWillMount() {
+    try {
+      await this.loadFonts();
+      console.log("font load successful");
+    } catch(e) {
+      console.log("Failed to load the font", e);
+    }
+  }
+
+  async loadFonts() {
+    await Font.loadAsync({
+        //Typography
+        IndieFlower: require("./assets/fonts/IndieFlower.ttf"),
+        SiviShapeIcons: require("./assets/fonts/SiviShapeIcons.ttf")
+    })
+  }
+
 }
 
 const styles = StyleSheet.create({
@@ -25,6 +48,12 @@ const styles = StyleSheet.create({
     fontSize: 20,
     textAlign: 'center',
     margin: 10,
+  },
+  quote:{
+    fontSize: 16,
+    fontFamily: "IndieFlower",
+    marginTop: 50,
+    padding: 20
   },
   instructions: {
     textAlign: 'center',
